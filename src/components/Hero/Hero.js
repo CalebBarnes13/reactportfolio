6a5dcc9d8446b6377ff6f43150c86335c148f47f
CalebBarnes13 <94274053+CalebBarnes13@users.noreply.github.com>
2022-08-17 11:58:13 -0400

    third commit

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -14,7 +14,7 @@ const Hero = (props) => (
       <SectionText>
         PRESS THE BUTTON BELOW TO LEARN MORE ABOUT ME!!!
       </SectionText>
-      <Button href="#about" onClick={props.handleClick}>Learn More</Button>
+      <a href="#about"><Button onClick={props.handleClick}>Learn More</Button></a>
     </LeftSection>
   </Section>
 );
